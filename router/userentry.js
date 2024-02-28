5b792b6d2769bbcc81d1def0c1c38ab26af87114
prajapathi deepak <94177698+ddeeppak@users.noreply.github.com>
2024-02-28 21:51:14 +0530

    Update userentry.js

--- router/userentry.js
+++ router/userentry.js
@@ -1,1 +1,16 @@
 
+const express = require('express');
+
+const router = express.Router();
+
+const database = require('../database/database');
+
+router.post('/', async (req,res,next) =>{
+    const {Name,Contact,mail} = req.body;
+    try{
+        database.query('insert into newuser values (?,?,?)',[Name,Contact,mail]);
+    }
+    catch (error) {
+        console.error("Error in UserEntry :",error);
+    }
+});
