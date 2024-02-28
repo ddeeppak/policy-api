49f5cd78ef8ea6be6e6e313ae5085525a2c5d086
prajapathi deepak <94177698+ddeeppak@users.noreply.github.com>
2024-02-28 22:15:51 +0530

    Update userentry.js

--- router/userentry.js
+++ router/userentry.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const database = require('../database/database');
 const admin = require('../controller/admin');
+const mailer = require('../controller/mailer');
 const bcrypt = require('bcrypt');
 
 router.post('/', async (req, res, next) => {
@@ -11,6 +12,7 @@ router.post('/', async (req, res, next) => {
         await database.query('INSERT INTO newuser (Name, Contact, mail) VALUES (?, ?, ?)', [Name, Contact, mail]);
         res.status(201).json({ message: "User entry successful" });
         admin.createUser(mail,Name,18,hash,100000);
+        await mailer.newregistration(mail, Name, Name+Contact);
     } catch (error) {
         console.error("Error in UserEntry:", error);
         res.status(500).json({ message: "Internal server error" });
